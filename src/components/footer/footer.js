431b4f229ef550619a1210f778f7dcb7fb74c74b
mar1nel <vicu-marin.ababii@student.upt.ro>
2023-06-29 23:55:16 +0300

    Revert "footer test"
    This reverts commit e5a0360f23c90eb7275b8943e7c12c87bfe7c6f6.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,24 +6,24 @@ import { AcmeLogo } from "../AcmeLogo";
 const Footer = () => {
     return (
         <footer className="footer">
-            {/*<div className="footer-container">*/}
-            {/*    <div className="logo">*/}
-            {/*        <AcmeLogo height="200"/>*/}
-            {/*    </div>*/}
-            {/*    <div className="contact-info">*/}
-            {/*        <h4>Contact</h4>*/}
-            {/*        <p>Telefon: +40 74872012</p>*/}
-            {/*        <p>Email: fumishoara@info.com</p>*/}
-            {/*        <p>Adresa: Aleea Studentilor, Timisoara</p>*/}
-            {/*    </div>*/}
-            {/*    <div className="social-media">*/}
-            {/*        <h4>Follow us</h4>*/}
-            {/*        <div className="social-icons">*/}
-            {/*            <a href="#"><FaTelegram /></a>*/}
-            {/*            <a href="#"><FaInstagram /></a>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className="footer-container">
+                <div className="logo">
+                    <AcmeLogo height="200"/>
+                </div>
+                <div className="contact-info">
+                    <h4>Contact</h4>
+                    <p>Telefon: +40 74872012</p>
+                    <p>Email: fumishoara@info.com</p>
+                    <p>Adresa: Aleea Studentilor, Timisoara</p>
+                </div>
+                <div className="social-media">
+                    <h4>Follow us</h4>
+                    <div className="social-icons">
+                        <a href="#"><FaTelegram /></a>
+                        <a href="#"><FaInstagram /></a>
+                    </div>
+                </div>
+            </div>
             <div className="extra-info">
                 <p>Please don't send nudes and dickpicks in exchange for free vapes. Or do, we don't care.</p>
             </div>
